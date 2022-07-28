6c8cbd27293b94fe97bd0b90da21388e2185a0c5
DashaSlesareva <zalezka@yandex.ru>
2022-07-28 15:20:45 +0300

    Manager added

--- src/main/java/ru/netology/domain/MovieManager.java
+++ src/main/java/ru/netology/domain/MovieManager.java
@@ -4,14 +4,44 @@ public class MovieManager {
 
     private Movies[] movies = new Movies[0];
 
-    public void add (Movies movie) {
+    public void add(Movies movie) {
         int length = movies.length + 1;
 
-        Movies[] tmp = new Movies [length];
-        for (int i = 0; i<movies.length; i++){
+        Movies[] tmp = new Movies[length];
+        for (int i = 0; i < movies.length; i++) {
             tmp[i] = movies[i];
         }
         tmp[tmp.length - 1] = movie;
         movies = tmp;
     }
+
+    public Movies[] findLast() {
+        int length;
+        if (movies.length > 10) {
+            length = 10;
+        } else length = movies.length;
+        Movies[] result = new Movies[length];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = movies[movies.length - i - 1];
+        }
+        return result;
+    }
+
+    public Movies[] findLast(int length) {
+        if (movies.length < length) {
+            length = movies.length;
+        }
+        Movies[] result = new Movies[length];
+        for (int i = 0; i < length; i++) {
+            result[i] = movies[movies.length - i - 1];
+        }
+        return result;
+    }
+
+
+    public Movies[] findAll() {
+        return movies;
+    }
+
+
 }
--- src/test/java/ru/netology/domain/MovieManagerTest.java
+++ src/test/java/ru/netology/domain/MovieManagerTest.java
@@ -1,6 +1,7 @@
 package ru.netology.domain;
 
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class MovieManagerTest {
@@ -9,19 +10,87 @@ public class MovieManagerTest {
     Movies second = new Movies(102, "Onward", "https://www.yandex.ru/film/567396", "cartoon", false);
     Movies third = new Movies(103, "OtelBelrad", "https://www.yandex.ru/film/675904", "comedy", false);
     Movies forth = new Movies(104, "TheGentelmen", "https://www.yandex.ru/film/775932", "action", false);
-    Movies fifth = new Movies(105, "TheInvisibleMan","https://www.yandex.ru/film/123456", "horror", false);
+    Movies fifth = new Movies(105, "TheInvisibleMan", "https://www.yandex.ru/film/123456", "horror", false);
     Movies sixth = new Movies(106, "TrollsWorldTour", "https://www.yandex.ru/film/907854", "cartoon", true);
     Movies seventh = new Movies(107, "NomerOdin", "https://www.yandex.ru/film/908986", "comedy", true);
+    Movies eighth = new Movies(108, "NomerOdin", "https://www.yandex.ru/film/908986", "comedy", true);
+    Movies ninth = new Movies(109, "NomerOdin", "https://www.yandex.ru/film/908986", "comedy", true);
+    Movies tenth = new Movies(110, "NomerOdin", "https://www.yandex.ru/film/908986", "comedy", true);
+    Movies eleventh = new Movies(111, "NomerOdin", "https://www.yandex.ru/film/908986", "comedy", true);
+    Movies twelfth = new Movies(112, "NomerOdin", "https://www.yandex.ru/film/908986", "comedy", true);
 
     @Test
-    public void addingTest () {
+    public void findAllTest() {
         MovieManager manager = new MovieManager();
         manager.add(first);
         manager.add(second);
         manager.add(third);
 
+        Movies[] actual = manager.findAll();
+        Movies[] expected = {first, second, third};
+        Assertions.assertArrayEquals(expected, actual);
+    }
 
+    @Test
+    public void findLastwithParameterTest() {
+        MovieManager manager = new MovieManager();
+        manager.add(first);
+        manager.add(second);
+        manager.add(third);
 
+        Movies[] actual = manager.findLast(2);
+        Movies[] expected = {third, second};
+        Assertions.assertArrayEquals(expected, actual);
+    }
 
+    @Test
+    public void findLastNoParameterTest1LessThan10() {
+        MovieManager manager = new MovieManager();
+        manager.add(first);
+        manager.add(second);
+        manager.add(third);
+
+        Movies[] actual = manager.findLast();
+        Movies[] expected = {third, second, first};
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void findLastNoParameterTestLengthMoreThan10() {
+        MovieManager manager = new MovieManager();
+        manager.add(first);
+        manager.add(second);
+        manager.add(third);
+        manager.add(forth);
+        manager.add(fifth);
+        manager.add(sixth);
+        manager.add(seventh);
+        manager.add(eighth);
+        manager.add(ninth);
+        manager.add(tenth);
+        manager.add(eleventh);
+        manager.add(twelfth);
+
+        Movies[] actual = manager.findLast();
+        Movies[] expected = {twelfth, eleventh, tenth, ninth, eighth, seventh, sixth, fifth, forth, third};
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void findLastNoParameterTestLengthIs10() {
+        MovieManager manager = new MovieManager();
+        manager.add(first);
+        manager.add(second);
+        manager.add(third);
+        manager.add(forth);
+        manager.add(fifth);
+        manager.add(sixth);
+        manager.add(seventh);
+        manager.add(eighth);
+        manager.add(ninth);
+        manager.add(tenth);
+        Movies[] actual = manager.findLast();
+        Movies[] expected = {tenth, ninth, eighth, seventh, sixth, fifth, forth, third, second, first};
+        Assertions.assertArrayEquals(expected, actual);
     }
 }
